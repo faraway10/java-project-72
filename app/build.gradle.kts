a1cdacce774a792dab0b038a49e3d85722856eb2
faraway10 <42tree42@gmail.com>
2024-09-20 04:27:29 +0300

    minimum working app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,13 @@
 plugins {
     id("java")
+    application
     checkstyle
     jacoco
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 group = "hexlet.code"
@@ -12,6 +18,12 @@ repositories {
 }
 
 dependencies {
+    implementation("io.javalin:javalin:6.3.0")
+    implementation("io.javalin:javalin-bundle:6.3.0")
+    implementation("io.javalin:javalin-rendering:6.3.0")
+    implementation("org.slf4j:slf4j-simple:2.0.16")
+    implementation("gg.jte:jte:3.1.12")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,28 @@
+package hexlet.code;
+
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+
+public class App {
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    public static void main(String[] args) {
+        var app = getApp();
+
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() {
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get("/", ctx -> ctx.result("Hello World"));
+
+        return app;
+    }
+}
